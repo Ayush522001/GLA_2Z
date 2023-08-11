3195b3ad9722ae34ebbc63f93ec648214592a0a9
Ayush-CB <130039413+Ayush-CB@users.noreply.github.com>
2023-08-11 15:27:14 +0530

    Mirror Patterns

---
+++ Lec_3/Pattern_15.java
@@ -0,0 +1,33 @@
+package Lec_3;
+
+public class Pattern_15 {
+    public static void main(String[] args) {
+        int n=5;
+
+        int row_no=1;
+        int cspace=0;
+        int cstar=n;
+        while (row_no<=(2*n-1)){
+            int space=1;
+            while (space<=cspace){
+                System.out.print("  ");
+                space=space+1;
+            }
+            int star=1;
+            while (star<=cstar){
+                System.out.print("* ");
+                star=star+1;
+            }
+            if (row_no<n){
+                cspace=cspace+2;
+                cstar=cstar-1;
+            }
+            else{
+                cspace=cspace-2;
+                cstar=cstar+1;
+            }
+            row_no=row_no+1;
+            System.out.println();
+        }
+    }
+}
