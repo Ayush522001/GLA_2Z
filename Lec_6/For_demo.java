2800ef618da80b78861b4d3a8bbe33ec5f996b25
Ayush-CB <130039413+Ayush-CB@users.noreply.github.com>
2023-08-23 10:56:38 +0530

    For loop demo

---
+++ Lec_6/For_demo.java
@@ -0,0 +1,15 @@
+package Lec_6;
+
+import java.util.Scanner;
+
+public class For_demo {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        System.out.println(n);
+
+//        for(int i=2;i<=20;i+=2){
+//            System.out.println(i);
+//        }
+    }
+}
